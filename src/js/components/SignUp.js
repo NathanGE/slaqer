120f00b25367acb83f0f677596386c4b3143b847
Nathan Elliott <nathangelliott@gmail.com>
2016-05-20 20:11:12 +0000

    Added ability to access form submission values

--- src/js/components/SignUp.js
+++ src/js/components/SignUp.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import {reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
 
-export default class SignUp extends React.Component {
+const getParams = (form, fields) => {
+  if(form === undefined) {
+    return {};
+  }
+  let params = {};
+  for(let field of fields) {
+    const fieldValue = form[field];
+    if(fieldValue) {
+      params[field] = fieldValue.value;
+    } else {
+      params[field] = '';
+    }
+  }
+  return params;
+};
+
+const mapStateToProps = (state) => {
+  return {
+    "signup" : getParams(state.form.signup, ['nickname', 'email', 'password'])
+  };
+}
+
+@connect(mapStateToProps);
+
+class SignUp extends React.Component {
+  submit(e) {
+    console.log(this.props.signup);
+    e.preventDefault();
+  }
   render() {
+    const {fields: {email, nickname, password}} = this.props;
+
     return (
       <div className="card col-xs-6 col-xs-offset-3">
         <h1 className="text-center">
           Sign up for Slaqer
         </h1>
 
-        <form className="sign-up-form text-center">
+        <form className="sign-up-form text-center" onSubmit={(e) => {this.submit(e);} }>
           <div className="col-xs-6 col-xs-offset-3">
             <label>Enter Your <strong>Nickname</strong></label>
-            <input type="text" className="form-control" placeholder="Nickname"/>
+            <input type="text" className="form-control" placeholder="Nickname" { ...nickname }/>
             <label>Enter Your <strong>Email</strong></label>
-            <input type="text" className="form-control" placeholder="Email"/>
+            <input type="text" className="form-control" placeholder="Email" { ...email } />
             <label>Enter Your <strong>Password</strong></label>
-            <input type="text" className="form-control" placeholder="Password"/>
+            <input type="text" className="form-control" placeholder="Password" { ...password } />
 
             <button type="submit" className="btn btn-block btn-primary">Create Account</button>
           </div>
@@ -28,3 +60,10 @@ export default class SignUp extends React.Component {
     );
   }
 }
+
+SignUp = reduxForm({
+  form: 'signup',
+  fields: ['nickname', 'email', 'password']
+})(SignUp);
+
+export default SignUp;

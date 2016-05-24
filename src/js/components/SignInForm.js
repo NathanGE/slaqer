0abde9af60f90c85ad30ae9d9bf99126620c57d3
Nathan Elliott <nathangelliott@gmail.com>
2016-05-24 20:32:12 +0000

    Add a sign in form

---
+++ src/js/components/SignInForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {reduxForm} from 'redux-form';
+import InputField from './InputField';
+
+class SignInForm extends React.Component {
+  render() {
+    const {fields: {email, nickname, password} } = this.props;
+    const errors = {};
+
+    return (
+      <div className="card col-xs-6 col-xs-offset-3">
+        <h1 className="text-center">Sign In to Slack</h1>
+          <form className="sign-up-form">
+            <div className="form-group text-center col-sm-6 col-sm-offset-3">
+              <InputField 
+                type="text"
+                placeholder="Email"
+                label={<span>Enter your <strong>email</strong></span>}
+                field={email}
+                errors={errors.email}
+              />
+
+              <InputField 
+                type="text"
+                placeholder="Password"
+                label={<span>Enter your <strong>password</strong></span>}
+                field={password}
+                errors={errors.password}
+              />
+            </div>
+
+            <br className="clear"/>
+
+            <div className="col-xs-6 col-xs-offset-3">
+              <button type="submit" className="btn btn-block btn-primary">Sign In</button>
+            </div>
+          </form>
+      </div>
+    );
+  }
+}
+
+SignInForm = reduxForm({
+  form: 'signin',
+  fields: ['email', 'password']
+})(SignInForm);
+
+export default SignInForm;
---
+++ src/js/containers/SignIn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignInForm from '../components/SignInForm';
+
+export default class SignIn extends React.Component {
+  render() {
+    return (
+      <SignInForm />
+    );
+  }
+}

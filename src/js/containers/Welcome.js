f54e28e61cc0fd0aae05472d65fe35821a4b392e
Nathan Elliott <nathangelliott@gmail.com>
2016-05-31 20:08:03 +0000

    Load data into the application from the api

--- src/js/containers/Welcome.js
+++ src/js/containers/Welcome.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import _ from 'lodash';
 import history from '../helpers/history';
+import axios from 'axios';
+import urlFor from '../helpers/urlFor';
+import {connect} from 'react-redux';
+
+@connect()
 
 export default class Welcome extends React.Component {
   componentWillMount() {
@@ -12,6 +17,13 @@ export default class Welcome extends React.Component {
     ];
 
     this.message =  _.sample(messages);
+
+    const url = urlFor("/users");
+    const result = axios.get(url);
+    this.props.dispatch({
+      type: "SET_USERS",
+      payload: result
+    });
   }
 
   componentDidMount() {

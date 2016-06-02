d040060076938bb421752ce3ba92ab8bc6088f8c
Nathan Elliott <nathangelliott@gmail.com>
2016-06-02 17:59:34 +0000

    hard coded in chat messages to check visual elements of message pane

--- src/js/components/MessagePane.js
+++ src/js/components/MessagePane.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import avatar from '../../img/avatar.png';
 
 export default class MessagePane extends React.Component {
   render() {
@@ -14,6 +15,66 @@ export default class MessagePane extends React.Component {
             <span className="activeUsers">{this.props.userCount}</span>
             <i className="glyphicon glyphicon-log-out" onClick={() => {this.props.signOut();}}></i>
           </div>
+
+          {/* hard coded messages */}
+          <div className="messageList row container-fluid">
+            
+            <div className="message row">
+              <div className="avatar">
+                <img src={avatar} />
+              </div>
+              <div className="text">
+                <div className="top-message">
+                  marco
+                  <span className="time">
+                    11:15AM
+                  </span>
+                </div>
+              </div>
+              <p>
+                Happy Monday Everyone
+              </p>
+              <p>
+                Hope everyone is awesome
+              </p>
+            </div>
+            
+            <div className="message row">
+              <div className="avatar">
+                <img src={avatar} />
+              </div>
+              <div className="text">
+                <div className="top-message">
+                  ken
+                  <span className="time">
+                    11:16AM
+                  </span>
+                </div>
+              </div>
+              <p>
+                Sup, Marco?
+              </p>
+            </div>
+
+            <div className="message row">
+              <div className="avatar">
+                <img src={avatar} />
+              </div>
+              <div className="text">
+                <div className="top-message">
+                  takehiro
+                  <span className="time">
+                    11:19AM
+                  </span>
+                </div>
+              </div>
+              <p>
+                Hello Everyone
+              </p>
+            </div>
+
+          </div>
+
         </div>
       </div>
     );

57c84da15a5daec31878c3e0bc6a77506872fb05
Nathan Elliott <nathangelliott@gmail.com>
2016-06-04 21:32:36 +0000

    Make the url in Heroku use the production API

--- src/js/helpers/publicKey.js
+++ src/js/helpers/publicKey.js
@@ -1,5 +1,10 @@
 const publicKey = () => {
-  return "33c638d9ddd88eac2fdc";
+  if(window.__webpack_env === "production") {
+    return "33c638d9ddd88eac2fdc";
+  } else {
+    return "93de78d30efb124b8e58";
+  }
+  
 }
 
 export default publicKey;
